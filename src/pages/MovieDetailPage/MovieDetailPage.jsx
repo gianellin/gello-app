cae9994214e93392dc01d2cf3159b08b97a373d5
gianellin <g.cevallos@gianellin.com>
2023-02-01 15:07:08 -0500

    create movieDetail page, change to semantic UI

--- src/pages/MovieDetailPage/MovieDetailPage.jsx
+++ src/pages/MovieDetailPage/MovieDetailPage.jsx
@@ -2,26 +2,19 @@
 import { Button, Form, Grid, Header, Image, Segment } from "semantic-ui-react";
 import { useState } from "react";
 import ErrorMessage from '../../components/ErrorMessage/ErrorMessage';
-
+import { Button, Form, Grid, Header, Image, Segment } from "semantic-ui-react";
 import { useNavigate } from "react-router-dom";
 // Since we want to make a request as a user about a user
 // we need to import the userService
 import React from 'react';
-import MovieDetailForm from '../../components/MovieDetailForm/MovieDetailForm';
-import MovieDetailInfo from '../../components/MovieDetailInfo/MovieDetailInfo';
+
 
 
 const MovieDetailPage = (props) => {
 	return (
 		<Main>
 			<section>
-				<MovieDetailInfo currentMovie={props.currentMovie} />
-				<MovieDetailForm
-					user={props.user}
-					currentMovie={props.currentMovie}
-					handleMovieDetailSubmit={props.handleMovieDetailSubmit}
-					handleMovieDetailUpdate={props.handleMovieDetailUpdate}
-				/>
+				
 			</section>
 		</Main>
 	);
